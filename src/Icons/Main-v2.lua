8255a08b52498b4be8dac6c4d5af988d089f016a
Txzp <tu_email@ejemplo.com>
2026-08-31 17:52:10 -0400

    Fix: Update icons to load from GitHub

--- src/Icons/Main-v2.lua
+++ src/Icons/Main-v2.lua
@@ -1,2 +1,8 @@
 -- Icons Main v2
-return {}
+-- Loaded from GitHub
+local HttpService = game:GetService("HttpService")
+local success, icons = pcall(function()
+	return HttpService:JSONDecode(game:HttpGet("https://raw.githubusercontent.com/Footagesus/Icons/main/Main-v2.json"))
+end)
+
+return success and icons or {}
